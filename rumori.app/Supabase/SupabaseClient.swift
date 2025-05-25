41c16c35efaf44ec33be9f43e3ca14bc26832c31
kianjain <kianpal731@gmail.com>
2025-05-25 12:34:45 +0200

    fix: update Supabase client configuration to use correct API

--- rumori.app/Supabase/SupabaseClient.swift
+++ rumori.app/Supabase/SupabaseClient.swift
@@ -7,7 +7,7 @@ class SupabaseManager {
     let client: SupabaseClient
     
     private init() {
-        let config = SupabaseClient.Configuration(
+        let options = SupabaseClientOptions(
             auth: .init(
                 autoRefreshToken: true,
                 persistSession: true,
@@ -19,7 +19,7 @@ class SupabaseManager {
         self.client = SupabaseClient(
             supabaseURL: URL(string: "https://ktgbpungskpjngopzarg.supabase.co")!,
             supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0Z2JwdW5nc2twam5nb3B6YXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1ODU1MjgsImV4cCI6MjA1OTE2MTUyOH0.OhnWtlSyX0bUze2mboyA0Mu9wzZt0JuiPNppy0Ph7To",
-            configuration: config
+            options: options
         )
     }
 } 
